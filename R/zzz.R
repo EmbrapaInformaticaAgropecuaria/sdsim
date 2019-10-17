da339f4e82b2fb3c0a3eb37eb3365e63974d2e52
Bruno Pereira <zehwallace@gmail.com>
2019-10-17 09:50:20 -0300

    added assigns to onLoad function for loading messages from XML at startup

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,20 @@
-.onLoad <- function(libname, pkgname) 
-{
+.onLoad <- function(libname, pkgname) {
   #options(digits = 10)  # increase digits precision
-  msgs <- loadMessages("inst/messages/messages.xml")
+  path = paste0(system.file(package = 'sdsim'), "/messages/messages.xml")
+  msgs <- loadMessages(path)
   
-  auxiliaryMsg <- msgs$auxiliaryMsg
-  constructorsMsg <- msgs$constructorsMsg
-  readInputDataMsg <- msgs$readInputDataMsg
-  sdCoupledModelMsg <- msgs$sdCoupledModelMsg
-  sdModelMsg <- msgs$sdModelMsg
-  sdOdeModelMsg <- msgs$sdOdeModelMsg
-  sdOutputMsg <- msgs$sdOutputMsg
-  sdScenarioMsg <- msgs$sdScenarioMsg
-  sdStaticModelMsg <- msgs$sdStaticModelMsg
-  sdSimulatorMsg <- msgs$sdSimulatorMsg
+  assign("auxiliaryMsg",      msgs$auxiliaryMsg, parent.env(environment()))
+  assign("constructorsMsg",   msgs$constructorsMsg, parent.env(environment()))
+  assign("readInputDataMsg",  msgs$readInputDataMsg, parent.env(environment()))
+  assign("sdCoupledModelMsg", msgs$sdCoupledModelMsg, parent.env(environment()))
+  assign("sdModelMsg",        msgs$sdModelMsg, parent.env(environment()))
+  assign("sdOutputMsg",       msgs$sdOutputMsg, parent.env(environment()))
+  assign("sdOdeModelMsg",     msgs$sdOdeModelMsg, parent.env(environment()))
+  assign("sdScenarioMsg",     msgs$sdScenarioMsg, parent.env(environment()))
+  assign("sdStaticModelMsg",  msgs$sdStaticModelMsg, parent.env(environment()))
+  assign("sdSimulatorMsg",    msgs$sdSimulatorMsg, parent.env(environment()))
 }
 
-.onUnload <- function(libname, pkgname)
-{
+.onUnload <- function(libname, pkgname) {
   #options(digits = 7)  # set default
 }
